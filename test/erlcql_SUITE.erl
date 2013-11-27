46067765a33c45d52c7095d21a28eedb7ac8accf
Krzysztof Rutka <krzysztof.rutka@gmail.com>
2013-11-27 09:54:30 +0100

    Fix tests

--- test/erlcql_SUITE.erl
+++ test/erlcql_SUITE.erl
@@ -4,17 +4,17 @@
 
 -include_lib("common_test/include/ct.hrl").
 
--define(HOST, "localhost").
--define(OPTS, []).
 -define(KEYSPACE, <<"erlcql_tests">>).
 
 -define(CREATE_KEYSPACE,
-        [<<"CREATE KEYSPACE ">>, ?KEYSPACE, <<" WITH replication = ",
-         "{'class': 'SimpleStrategy', 'replication_factor': 1}">>]).
--define(DROP_KEYSPACE, [<<"DROP KEYSPACE ">>, ?KEYSPACE]).
--define(USE_KEYSPACE, [<<"USE ">>, ?KEYSPACE]).
+        [<<"CREATE KEYSPACE erlcql_tests WITH replication = ",
+           "{'class': 'SimpleStrategy', 'replication_factor': 1}">>]).
+-define(DROP_KEYSPACE, <<"DROP KEYSPACE erlcql_tests">>).
+-define(USE_KEYSPACE, <<"USE erlcql_tests">>).
 -define(CREATE_TABLE, <<"CREATE TABLE t (k int PRIMARY KEY, v text)">>).
 -define(DROP_TABLE, <<"DROP TABLE t">>).
+
+-import(erlcql, [q/2, q/3]).
 
 %% Fixtures -------------------------------------------------------------------
 
@@ -23,7 +23,7 @@ suite() ->
      {timetrap, {seconds, 60}}].
 
 init_per_suite(Config) ->
-    {ok, Pid} = erlcql_client:start_link(?HOST, ?OPTS),
+    {ok, Pid} = erlcql:start_link(),
     unlink(Pid),
     q(Pid, ?DROP_KEYSPACE),
     [{pid, Pid} | Config].
@@ -35,7 +35,7 @@ end_per_suite(Config) ->
 init_per_group(data_tests, Config) ->
     Pid = get_pid(Config),
     q(Pid, ?CREATE_KEYSPACE),
-    q(Pid, ?USE_KEYSPACE),
+    {ok, ?KEYSPACE} = q(Pid, ?USE_KEYSPACE),
     q(Pid, ?CREATE_TABLE),
     Config;
 init_per_group(_GroupName, Config) ->
@@ -43,7 +43,7 @@ init_per_group(_GroupName, Config) ->
 
 end_per_group(data_tests, Config) ->
     Pid = get_pid(Config),
-    q(Pid, ?DROP_KEYSPACE);
+    {ok, dropped} = q(Pid, ?DROP_KEYSPACE);
 end_per_group(_GroupName, _Config) ->
     ok.
 
@@ -56,7 +56,7 @@ init_per_testcase(_TestCase, Config) ->
 
 end_per_testcase(create_keyspace, Config) ->
     Pid = get_pid(Config),
-    q(Pid, ?DROP_KEYSPACE);
+    {ok, dropped} = q(Pid, ?DROP_KEYSPACE);
 end_per_testcase(_TestCase, _Config) ->
     ok.
 
@@ -88,7 +88,7 @@ drop_keyspace(Config) ->
 insert(Config) ->
     Pid = get_pid(Config),
     {ok, void} = q(Pid, <<"INSERT INTO t (k, v) VALUES (1, 'one')">>),
-    Rows = q(Pid, <<"SELECT * FROM t">>),
+    Rows = q(Pid, <<"SELECT * FROM t">>, one),
     {ok, {[[<<0, 0, 0, 1>>, <<"one">>]],
           [{<<"k">>, int}, {<<"v">>, varchar}]}} = Rows.
 
@@ -97,10 +97,7 @@ insert(Config) ->
 get_pid(Config) ->
     proplists:get_value(pid, Config).
 
-q(Pid, String) ->
-    erlcql_client:query(Pid, String, one).
-
 get_keyspaces(Pid) ->
     Check = <<"SELECT keyspace_name FROM system.schema_keyspaces">>,
-    {ok, {Keyspaces, _}} = q(Pid, Check),
+    {ok, {Keyspaces, _}} = q(Pid, Check, one),
     Keyspaces.
